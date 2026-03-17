7c3b6bc230c2378bf5cb894bf409e16ab44b57a6
fell-z <matheusfelipeagapito@hotmail.com>
2026-03-16 22:39:00 -0300

    Add rspec-rails, shoulda matchers and factory bot gems

---
+++ spec/rails_helper.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+# Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
+# that will avoid rails generators crashing because migrations haven't been run yet
+# return unless Rails.env.test?
+require 'rspec/rails'
+
+# Add additional requires below this line. Rails is not loaded until this point!
+require_relative "support/factory_bot"
+require_relative "support/shoulda_matchers"
+
+# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+
+# Ensures that the test database schema matches the current schema file.
+# If there are pending migrations it will invoke `db:test:prepare` to
+# recreate the test database by loading the schema.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+  config.filter_rails_from_backtrace!
+end
