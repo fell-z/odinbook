21a37b577df11308f27de9cdcd13d94843926371
fell-z <matheusfelipeagapito@hotmail.com>
2026-03-21 12:34:13 -0300

    Fix valid/invalid user authentication tests
    - Stop using FactoryBot in user factory
      to allow more control over the test data.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "foo#{n}" }
-    email { Faker::Internet.email }
+    email { "#{name.downcase}@example.com" }
     password { "test_password" }
     password_confirmation { "test_password" }
   end
--- spec/requests/users/sessions_spec.rb
+++ spec/requests/users/sessions_spec.rb
@@ -11,19 +11,26 @@ RSpec.describe "User Sessions", type: :request do
   end
 
   describe "POST /users/sign_in" do
+    before { create(:user, name: "John") }
+    let(:user_credentials) { attributes_for(:user, name: "John", password:) }
+
     context "when the user credentials are valid" do
+      let(:password) { "test_password" }
+
       it "redirects to the root" do
         pending("root path to defined")
 
-        post user_session_path, params: { user: attributes_for(:user) }
+        post user_session_path, params: { user: user_credentials }
 
         expect(response).to redirect_to(root_path)
       end
     end
 
     context "when the user credentials are invalid" do
+      let(:password) { "wrong_password" }
+
       it "renders the 'new' form again" do
-        post user_session_path, params: { user: attributes_for(:user, password: "wrong_password") }
+        post user_session_path, params: { user: user_credentials }
 
         expect(response).to have_http_status(:unprocessable_content)
         expect(response.body).to include("Log in")
