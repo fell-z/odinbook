0fc556778c8ede7926eadcfea41a6a75fa4c436e
fell-z <matheusfelipeagapito@hotmail.com>
2026-03-22 23:28:56 -0300

    Make users able to create/edit posts and create comments
    - Also changed a bunch of div/p tags to
      more semantic ones like section and nav.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,42 @@ class PostsController < ApplicationController
 
     @posts = Post.includes(:user).recent.page(@page_number)
   end
+
+  def show
+    @post = Post.includes(comments: [ :user ]).find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [ :body ])
+  end
 end
